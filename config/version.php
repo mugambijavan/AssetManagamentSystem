37df601b4d9becdcc113361c6efb9bc26c28c8b1
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-03-09 15:08:58 +0300

    ASM

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.3.1',
+  'full_app_version' => 'v6.3.1 - build 12672-g00cea3eb3',
+  'build_version' => '12672',
+  'prerelease_version' => '',
+  'hash_version' => 'g00cea3eb3',
+  'full_hash' => 'v6.3.1-180-g00cea3eb3',
+  'branch' => 'master',
+);
